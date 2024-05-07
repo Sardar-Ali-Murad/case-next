0a1ddb3cba2809ab6d557f87f7ac8cb4749981d3
Sardar-Ali-Murad <mar389786@gmail.com>
2024-05-07 05:32:11 -0700

    first commit

---
+++ app/api/sentry-example-api/route.js
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
+export function GET() {
+  throw new Error("Sentry Example API Route Error");
+  return NextResponse.json({ data: "Testing Sentry Error..." });
+}
